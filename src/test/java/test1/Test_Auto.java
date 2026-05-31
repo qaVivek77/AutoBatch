7e6b3194fea02c58115dd064d12cc319d5a9e311
vivek <qatester7767@gmail.com>
2026-05-31 12:16:58 +0530

    2nd commit for new changes in project

---
+++ src/test/java/test1/Test_Auto.java
@@ -0,0 +1,12 @@
+package test1;
+
+public abstract class Test_Auto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Repo Test 2");
+	}
+
+}
